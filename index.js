5ddf5383f9d25d01c46571cbb6b2fe547ce6d8bb
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-22 17:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
